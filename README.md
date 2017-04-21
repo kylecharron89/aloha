8984d8a11efec9cad76a27f75d6158d5647244af
Kyle Charron <kceteam@gmail.com>
2017-04-21 12:02:03 -0400

    Added carousel

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # aloha
 Aloha Clothing Website
+Built by Kyle Charron with HTML/CSS/JS
+
+Optimized for mobile/desktop browsing.
+
--- flickity.js
+++ flickity.js
@@ -1,12 +1,7 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
+$(document).ready(function (){
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'center',
+    contain: true
+  });
+})
